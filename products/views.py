0f5669258a4f7cf84c1c1a01edc68f21efb783fe
Muhammed Fayis P <mhdfaaiz803@gmail.com>
2024-07-15 18:59:39 +0530

    home layout designed and succesfully got output,currently stopped while working on creating the product page..it is on process

--- products/views.py
+++ products/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def fan():
-    print("hii")
+def index(request):
+    return render(request,'index.html')
+
+def list_products(request):
+    return render(request,'products.html')
+
+def detail_products(request):
+    return render(request,'products.html')
