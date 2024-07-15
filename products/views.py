1a609f7f9307f1d757384f47f15abbc669c9850c
Muhammed Fayis P <mhdfaaiz803@gmail.com>
2024-07-16 00:59:18 +0530

    succesfully created product page and working good

--- products/views.py
+++ products/views.py
@@ -5,6 +5,14 @@ def index(request):
     return render(request,'index.html')
 
 def list_products(request):
+    """"_summary_
+    return product list page
+    Args:
+        request (_type_): _description_
+        
+    Returns:
+        _types_: _description_
+    """
     return render(request,'products.html')
 
 def detail_products(request):
