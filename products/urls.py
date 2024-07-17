bd1e4dc33a2a53781624ee5219fe715b5d6c261e
Muhammed Fayis P <mhdfaaiz803@gmail.com>
2024-07-17 14:09:02 +0530

    some modification performed on the design

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('product_list',views.list_products,name='list_products'),
+    path('product_details',views.detail_products,name='detail_products')
+]
