aad6443f575b449ed6d5eba9f5f982d933e82eae
Muhammed Fayis P <mhdfaaiz803@gmail.com>
2024-07-19 00:00:49 +0530

    login and register form on process..cuurently stoping on regiser form..only fields added

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('product_list',views.list_products,name='list_products'),
-    path('product_details',views.detail_products,name='detail_products')
+    path('product_details/<pk>',views.detail_products,name='detail_products')
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from . models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    featured_product=Product.objects.order_by('priority')[:4]
+    latest_product=Product.objects.order_by('-id')[:4]
+    context={
+        'featured_product':featured_product,
+        'latest_product':latest_product
+    }
+    return render(request,'index.html',context)
 
 def list_products(request):
     """"_summary_
@@ -14,9 +21,16 @@ def list_products(request):
     Returns:
         _types_: _description_
     """
-    product_list=Product.objects.all()
+    page=1
+    if request.GET:
+        page=request.GET.get('page',1)
+    product_list=Product.objects.order_by('priority')
+    product_paginator=Paginator(product_list,8)
+    product_list=product_paginator.get_page(page)
     context={ 'products' : product_list}
     return render(request,'products.html',context)
 
-def detail_products(request):
-    return render(request,'product_details.html')
+def detail_products(request,pk):
+    product=Product.objects.get(pk=pk)
+    context={'product':product}
+    return render(request,'product_details.html',context )
