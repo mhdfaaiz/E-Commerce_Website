8bb520a5af6a7b7ca39f99cded4166bcba25cda7
Muhammed Fayis P <mhdfaaiz803@gmail.com>
2024-07-20 00:09:15 +0530

    bad day...not moved and not cleared error..tommorrow i will ...bye then

--- customer/models.py
+++ customer/models.py
@@ -7,7 +7,6 @@ class Customer(models.Model):
     DELETE=0
     DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
     name=models.CharField(max_length=200)
-    address=models.TextField()
     User=models.OneToOneField(User,on_delete=models.CASCADE,related_name='customer_profile')
     phone=models.CharField(max_length=10)
     delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import Customer
 # Create your views here.
 def account(request):
+    if request.POST and 'register' in request.POST:
+        try:
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            email=request.POST.get('email')
+            phone=request.POST.get('phone')
+            
+            #creates user accounts
+            user=User.objects.create(
+                username=username,
+                password=password,
+                email=email
+            )
+            #creates customer account
+            customer=Customer.objects.create(
+                user=user,
+                phone=phone,
+            )
+            return redirect('home')
+        except Exception as e:
+            error_message="Duplicate username or invalid credentials"
     return render(request,'account.html')
